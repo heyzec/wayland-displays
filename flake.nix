467b2973626e3a01c5dbbafb12306aa8114dd521
heyzec <61238538+heyzec@users.noreply.github.com>
2025-05-21 23:36:31 +0800

    build: use Meson instead of CMake TODO fix comment
    motivation: I can't figure out how to get OpenGL libraries to play nice
    with GTK3 when using CMake, but it somehow works with Meson

--- flake.nix
+++ flake.nix
@@ -5,119 +5,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
       ];
-      perSystem = { config, self', inputs', pkgs, lib, system, ... }:
-        let
-          wayland-displays = pkgs.stdenv.mkDerivation
-            {
-              name = "wayland-displays";
-
-              # Call Makefile targets instead of CMake
-              dontUseCmakeConfigure = true;
-              enableParallelBuilding = true; # dontUseCmakeConfigure seems to set this to false
-
-              # Filtered list of source files
-              src = lib.sourceByRegex ./. [
-                "Makefile"
-                "CMakeLists.txt"
-                "^src.*"
-                "^lib.*"
-                "^cmake.*"
-                "^protocols.*"
-                "^resources.*"
-                "^tests/unit.*"
-              ];
-
-              # Needed at compile time
-              nativeBuildInputs = with pkgs; [
-                # C++ Compiler is already part of stdenv
-                cmake
-                pkg-config
-                (gtk3.overrideAttrs (old: {
-                  mesonFlags = old.mesonFlags ++ [
-                    "--buildtype=debug"
-                  ];
-                  env.NIX_CFLAGS_COMPILE = "";
-                }))
-                cairo
-                wayland
-                wayland-scanner
-                yaml-cpp
-                catch2_3
-                spdlog
-                libepoxy
-                wrapGAppsHook3
-              ];
-
-              buildInputs = [ ];
-
-              makeFlags = [
-                "PREFIX=${placeholder "out"}"
-              ];
-
-              doCheck = true;
-            };
-
-          wayland-displays-debug = (config.packages.default.overrideAttrs {
-            # See https://nixos.wiki/wiki/Debug_Symbols
-            dontStrip = true;
-            separateDebugInfo = true;
-          });
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs;
-              [
-                # For clangd
-                clang-tools_17
-
-                # Create .ui files
-                glade
-
-                # Testing sockets
-                socat
-
-                # Experiment with existing tools
-                way-displays
-                wdisplays
-                nwg-displays
-                kanshi
-              ] ++ config.packages.default.nativeBuildInputs;
-          };
-
-          packages = {
-            default = wayland-displays;
-
-            # Run with nix -L build .#test-vm
-            test-vm = pkgs.testers.runNixOSTest (import ./tests/vm/test.nix wayland-displays);
-          };
-
-          apps.debug =
-            let
-              debug = pkgs.writeShellApplication {
-                name = "debug";
-                text = ''
-                  trap "" SIGUSR1
-                  # readelf --debug-dump=line  ${config.packages.default.out}/bin/wayland-displays
-                  ${pkgs.gdb}/bin/gdb \
-                    --quiet \
-                    --ex "handle SIGUSR1 nostop pass" \
-                    --ex "run" \
-                    --args ${wayland-displays-debug}/bin/wayland-displays "$@"
-                '';
-              };
-            in
-            {
-              type = "app";
-              # Using a derivation in here gets replaced
-              # with the path to the built output
-              program = "${debug}/bin/debug";
-            };
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        wayland-displays = pkgs.callPackage ./package.nix {};
+        wayland-displays-debug = wayland-displays.overrideAttrs {
+          # See https://nixos.wiki/wiki/Debug_Symbols
+          dontStrip = true;
+          separateDebugInfo = true;
         };
+      in {
+        packages.default = wayland-displays;
+
+        packages.debug = pkgs.writeShellApplication {
+          name = "debug";
+          text = ''
+            trap "" SIGUSR1
+            ${pkgs.gdb}/bin/gdb \
+              --quiet \
+              --ex "handle SIGUSR1 nostop pass" \
+              --ex "run" \
+              --args ${wayland-displays-debug}/bin/wayland-displays "$@"
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              # For clangd
+              clang-tools_17
+              # Create .ui files
+              glade
+              # Testing sockets
+              socat
+            ]
+            ++ config.packages.default.nativeBuildInputs
+            ++ config.packages.default.buildInputs;
+        };
+      };
     };
 }
