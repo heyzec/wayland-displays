65312941694a9c0d3548976e21a85f01415dfcee
heyzec <61238538+heyzec@users.noreply.github.com>
2025-05-21 14:39:18 +0800

    temp

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <gtk/gtk.h>
+
+//
+// Should provide the active view for a GTK application
+//
+static void activate(GtkApplication *app, gpointer user_data) {
+  GtkWidget *window;
+  GtkWidget *label;
+
+  window = gtk_application_window_new(app);
+  label = gtk_label_new("Hello GNOME!");
+  gtk_container_add(GTK_CONTAINER(window), label);
+  gtk_window_set_title(GTK_WINDOW(window), "Welcome to GNOME");
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+  gtk_widget_show_all(window);
+} // end of function activate
+
+//
+// main is where all program execution starts
+//
+int main(int argc, char **argv) {
+  GtkApplication *app;
+  int status;
+
+#if GLIB_CHECK_VERSION(2, 74, 0)
+  app = gtk_application_new(NULL, G_APPLICATION_DEFAULT_FLAGS);
+#else
+  app = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+#endif
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
+} // end of function main
