heyzec/wayland-displays

commit fb0bc193378762a5769ced4ec96950674e48c7ac

fix(gui): don't show disabled displays

src/gui/canvas.cpp
@@ -28,7 +28,7 @@ const float CANVAS_FAC = 0.15;
 
 static GtkWidget *canvas;
 static CanvasState *state;
-static void (*on_canvas_updated)(int, std::vector<Box>);
+static void (*on_canvas_updated)(std::string, std::vector<Box>);
 
 // ============================================================
 // Helpers
@@ -184,7 +184,7 @@ void on_drag_start(GtkGestureDrag *drag_, gdouble start_x, gdouble start_y, gpoi
       break;
     }
   }
-  on_canvas_updated(state->selected_box, state->boxes);
+  on_canvas_updated(state->boxes.at(state->selected_box).name, state->boxes);
 
   queue_draw_area(canvas);
 }
@@ -215,7 +215,7 @@ void on_drag_update(GtkGestureDrag *drag_, gdouble delta_x, gdouble delta_y, gpo
   box->x = MIN(MAX(x, 0), canvas_width / CANVAS_FAC - box->width);
   box->y = MIN(MAX(y, 0), canvas_height / CANVAS_FAC - box->height);
 
-  on_canvas_updated(state->selected_box, state->boxes);
+  on_canvas_updated(state->boxes.at(state->selected_box).name, state->boxes);
 
   queue_draw_area(canvas);
 }
@@ -249,6 +249,6 @@ void refresh_canvas(std::vector<Box> boxes) {
   queue_draw_area(canvas);
 }
 
-void attach_canvas_updated_callback(void (*func)(int, std::vector<Box>)) {
+void attach_canvas_updated_callback(void (*func)(std::string, std::vector<Box>)) {
   on_canvas_updated = func;
 }

src/gui/box.hpp
@@ -1,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+struct Box {
+  std::string name;
+
+  float x;
+  float y;
+  float width;
+  float height;
+
+  bool within(float pt_x, float pt_y) {
+    return x <= pt_x && pt_x <= x + width && y <= pt_y && pt_y <= y + height;
+  }
+};

src/gui/canvas.hpp
@@ -1,19 +1,10 @@
+#include "gui/box.hpp"
+
 #include <gtk/gtk.h>
 #include <vector>
-
-struct Box {
-  float x;
-  float y;
-  float width;
-  float height;
-
-  bool within(float pt_x, float pt_y) {
-    return x <= pt_x && pt_x <= x + width && y <= pt_y && pt_y <= y + height;
-  }
-};
 
 void setup_canvas(GtkDrawingArea *drawing_area, std::vector<Box> boxes);
 
 void refresh_canvas(std::vector<Box> boxes);
 
-void attach_canvas_updated_callback(void (*func)(int, std::vector<Box>));
+void attach_canvas_updated_callback(void (*func)(std::string, std::vector<Box>));

src/gui/details.cpp
@@ -41,6 +41,7 @@ static void (*on_details_updated)(int, DisplayInfo) = [](int, DisplayInfo) {};
 // Only allow these globals to be modified from gui.cpp!
 // TODO: Move these back to gui.cpp
 vector<DisplayInfo> displays;
+/* Index of selected DisplayInfo */
 int selected_display = 0;
 
 // ============================================================

src/gui/gui.cpp
@@ -35,7 +35,13 @@ template <class T> using vector = std::vector<T>;
 vector<Box> create_boxes_from_displays(vector<DisplayInfo> displays) {
   auto boxes = vector<Box>();
   for (auto display : displays) {
+    if (!display.enabled) {
+      continue;
+    }
+
     Box box = Box{};
+    box.name = display.name;
+
     box.x = display.pos_x;
     box.y = display.pos_y;
 
@@ -61,17 +67,30 @@ vector<Box> create_boxes_from_displays(vector<DisplayInfo> displays) {
 }
 
 void update_displays_from_boxes(vector<DisplayInfo> *displays, const vector<Box> boxes) {
-  if (displays->size() != boxes.size()) {
-    log_warn("Vector sizes do not match!\n");
-    return;
-  }
+  // Account for when size of boxes less than displays (because some displays are disabled)
+  auto it_display = displays->begin();
+  auto it_box = boxes.begin();
 
-  // Note that only position attributes are updated back
-  for (int i = 0; i < boxes.size(); i++) {
-    Box box = boxes.at(i);
-    DisplayInfo *display = &displays->at(i);
+  while (it_box != boxes.end()) {
+    if (it_display == displays->end()) {
+      log_warn("Assertion failed: Trying to update display {} but not found!",
+               it_box->name.c_str());
+      return;
+    }
+    DisplayInfo *display = &(*it_display);
+    Box box = *it_box;
+    if (display->name != box.name) {
+      ++it_display;
+      continue;
+    }
+
+    // Note that only position attributes are updated back
     display->pos_x = box.x;
     display->pos_y = box.y;
+
+    // Move to next elements
+    ++it_display;
+    ++it_box;
   }
 }
 
@@ -152,9 +171,14 @@ void setup_gui() {
   // Update content values in window
   refresh_gui();
 
-  attach_canvas_updated_callback([](int selected_box, const vector<Box> boxes) {
+  attach_canvas_updated_callback([](string selected_box, const vector<Box> boxes) {
     update_displays_from_boxes(&displays, boxes);
-    selected_display = selected_box;
+    for (int i = 0; i < displays.size(); i++) {
+      if (displays.at(i).name == selected_box) {
+        selected_display = i;
+        break;
+      }
+    }
     refresh_gui();
   });
 

commit 0656ef759cf49b6d3c326701b34f6f2dbc2483af

fix(gui): don't crash when clicking outside boxes
regression introduced in fb0bc193378762a5769ced4ec96950674e48c7ac

src/gui/canvas.cpp
@@ -182,8 +182,11 @@ void on_drag_start(GtkGestureDrag *drag_, gdouble start_x, gdouble start_y, gpoi
       break;
     }
   }
-  on_canvas_updated(state->boxes.at(state->selected_box).name, state->boxes);
+  if (state->selected_box == -1) {
+    return;
+  }
 
+  on_canvas_updated(state->boxes.at(state->selected_box).name, state->boxes);
   queue_draw_area(canvas);
 }
 